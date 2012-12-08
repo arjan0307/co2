055b384949096151cad21f9d4b534f5a432d7556
Arjan <arjan0307@gmail.com>
2012-12-08 13:30:08 +0100

    Set-up a new rails project with postgresql.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Co2::Application.config.secret_token = '008b819a1a732e79958736f00cd7524e78ad7aff4d06d510899d86574ccb0e9bca44116523911d935cf27442a0ce7c42306e514683eb683723462dfcac14594b'
